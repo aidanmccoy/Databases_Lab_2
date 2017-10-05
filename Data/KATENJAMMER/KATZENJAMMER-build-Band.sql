128130e8b152f41d3b4265a2c3277311bf4e6eb5
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-05 03:29:16 -0700

    Created sql files for katzenjammer

---
+++ Data/KATENJAMMER/KATZENJAMMER-build-Band.sql
@@ -0,0 +1,5 @@
+INSERT INTO Band(Id,Firstname,Lastname) VALUES (1,'''Solveig''','''Heilo''');
+INSERT INTO Band(Id,Firstname,Lastname) VALUES (2,'''Marianne''','''Sveen''');
+INSERT INTO Band(Id,Firstname,Lastname) VALUES (3,'''Anne-Marit''','''Bergheim''');
+INSERT INTO Band(Id,Firstname,Lastname) VALUES (4,'''Turid''','''Jorgensen''');
+
