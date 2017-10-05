161884874ecd5b2ca98377b2c33b43d5543198ee
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-05 02:00:37 -0700

    airports

--- AIRLINES-setup.sql
+++ AIRLINES-setup.sql
@@ -1,23 +1,23 @@
 -- aimccoy
 
 CREATE TABLE Airlines (
-	ID CHAR(2) PRIMARY KEY,
+	Id CHAR(2) PRIMARY KEY,
 	Airline VARCHAR(100),
-	AirlineAbr VARCHAR(10),
+	Abbreviation VARCHAR(10),
 	Country VARCHAR(100)
 );
 
 CREATE TABLE Airports (
 	City VARCHAR(100),
-	Code CHAR(3) PRIMARY KEY,
-	Name VARCHAR(100),
+	AirportCode CHAR(3) PRIMARY KEY,
+	AirportName VARCHAR(100),
 	Country VARCHAR(100),
-	CountryAbr VARCHAR(100)
+	CountryAbbrev VARCHAR(100)
 );
 
 CREATE TABLE Flights (
 	Airline VARCHAR(100),
 	FlightNo VARCHAR(100) PRIMARY KEY,
-	Source CHAR(3),
-	Dest CHAR(3)
+	SourceAirport CHAR(3),
+	DestAirport CHAR(3)
 );
