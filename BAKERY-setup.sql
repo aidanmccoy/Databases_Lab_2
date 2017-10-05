70a54200439cb6a836c1c2bd5fb38b37b1aa56be
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-05 11:27:47 -0700

    refactoring bakery setup

--- BAKERY-setup.sql
+++ BAKERY-setup.sql
@@ -2,26 +2,28 @@
 
 CREATE TABLE Customers (
 	ID INTEGER PRIMARY KEY,
-	Lastname VARCHAR(15),
-	Firstname VARCHAR(15)
+	Lastname VARCHAR(15) NOT NULL,
+	Firstname VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE Goods (
 	ID VARCHAR(20) PRIMARY KEY,
-	Flavor VARCHAR(20),
-	Food VARCHAR(20),
-	Price NUMERIC(5,2)
-);
-
-CREATE TABLE Items (
-	Reciept VARCHAR(50),
-	Ordinal VARCHAR(100),
-	Item CHAR(2)
+	Flavor VARCHAR(20) NOT NULL,
+	Food VARCHAR(20) NOT NULL,
+	Price NUMERIC(5,2) NOT NULL
 );
 
 CREATE TABLE Receipts (
-	RecieptNumber VARCHAR(100) PRIMARY KEY,
-	Date DATE,
-	CustomerID CHAR(2)
+	RecieptNumber INTEGER PRIMARY KEY,
+	Date DATE NOT NULL,
+	CustomerID INTEGER NOT NULL
+);
+
+CREATE TABLE Items (
+	Reciept INTEGER NOT NULL,
+	Ordinal INTEGER NOT NULL,
+	Item VARCHAR(20) PRIMARY KEY
+	FOREIGN KEY Reciept REFERENCES Receipts (RecieptNumber),
+	FOREIGN KEY Item REFERENCES Goods (ID)
 );
 
