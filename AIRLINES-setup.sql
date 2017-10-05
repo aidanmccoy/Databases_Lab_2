5465a431ff513fbaa6b880a46f2563bd1ecca324
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-04 23:51:55 -0700

    Changed formatting to match specs

--- AIRLINES-setup.sql
+++ AIRLINES-setup.sql
@@ -1,20 +1,23 @@
-#Aidan McCoy
+-- aimccoy
 
 CREATE TABLE Airlines (
-ID CHAR(2) PRIMARY KEY,
-Airline VARCHAR(100),
-AirlineAbr VARCHAR(10),
-Country VARCHAR(100));
+	ID CHAR(2) PRIMARY KEY,
+	Airline VARCHAR(100),
+	AirlineAbr VARCHAR(10),
+	Country VARCHAR(100)
+);
 
 CREATE TABLE Airports (
-City VARCHAR(100),
-Code CHAR(3) PRIMARY KEY,
-Name VARCHAR(100),
-Country VARCHAR(100),
-CountryAbr VARCHAR(100));
+	City VARCHAR(100),
+	Code CHAR(3) PRIMARY KEY,
+	Name VARCHAR(100),
+	Country VARCHAR(100),
+	CountryAbr VARCHAR(100)
+);
 
 CREATE TABLE Flights (
-Airline VARCHAR(100),
-FlightNo VARCHAR(100) PRIMARY KEY,
-Source CHAR(3),
-Dest CHAR(3));
+	Airline VARCHAR(100),
+	FlightNo VARCHAR(100) PRIMARY KEY,
+	Source CHAR(3),
+	Dest CHAR(3)
+);
--- BAKERY-setup.sql
+++ BAKERY-setup.sql
@@ -1,23 +1,27 @@
-#Aidan McCoy
+-- aimccoy
 
 CREATE TABLE Customers (
-ID CHAR(2) PRIMARY KEY,
-Lastname VARCHAR(100),
-Firstname VARCHAR(100));
+	ID CHAR(2) PRIMARY KEY,
+	Lastname VARCHAR(100),
+	Firstname VARCHAR(100)
+);
 
 CREATE TABLE Goods (
-ID CHAR(2) PRIMARY KEY,
-Flavor VARCHAR(100),
-Food VARCHAR(100),
-Price VARCHAR(100));
+	ID CHAR(2) PRIMARY KEY,
+	Flavor VARCHAR(100),
+	Food VARCHAR(100),
+	Price VARCHAR(100)
+);
 
 CREATE TABLE Items (
-Receipt VARCHAR(50) PRIMARY KEY,
-Ordinal VARCHAR(100),
-Item CHAR(2));
+	Receipt VARCHAR(50) PRIMARY KEY,
+	Ordinal VARCHAR(100),
+	Item CHAR(2)
+);
 
 CREATE TABLE Receipts (
-ReceiptNumber VARCHAR(100) PRIMARY KEY,
-Date DATE,
-CustomerID CHAR(2));
+	ReceiptNumber VARCHAR(100) PRIMARY KEY,
+	Date DATE,
+	CustomerID CHAR(2)
+);
 
