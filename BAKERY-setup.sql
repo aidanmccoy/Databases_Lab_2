2430ad28cff8d097d2e8f799163dea99c5dc7f9e
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-05 11:20:35 -0700

    refactoring bakery setup

--- BAKERY-setup.sql
+++ BAKERY-setup.sql
@@ -1,16 +1,16 @@
 -- aimccoy
 
 CREATE TABLE Customers (
-	ID CHAR(2) PRIMARY KEY,
-	Lastname VARCHAR(100),
-	Firstname VARCHAR(100)
+	ID INTEGER PRIMARY KEY,
+	Lastname VARCHAR(15),
+	Firstname VARCHAR(15)
 );
 
 CREATE TABLE Goods (
-	ID CHAR(20) PRIMARY KEY,
-	Flavor VARCHAR(100),
-	Food VARCHAR(100),
-	Price VARCHAR(100)
+	ID VARCHAR(20) PRIMARY KEY,
+	Flavor VARCHAR(20),
+	Food VARCHAR(20),
+	Price NUMERIC(5,2)
 );
 
 CREATE TABLE Items (
