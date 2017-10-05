ffd15da2cfc4e8821e347fd6615f5bb81da6622f
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-05 03:41:27 -0700

    Created sql file for marathon

--- MARATHON-setup.sql
+++ MARATHON-setup.sql
@@ -4,13 +4,13 @@ CREATE TABLE Marathon (
 	Place VARCHAR(10),
 	Time VARCHAR(10),
 	Pace VARCHAR(10),
-	GrpPlace VARCHAR(10),
+	GroupPlace VARCHAR(10),
 	Grp VARCHAR(10),
 	Age CHAR(2),
 	Sex CHAR(1),
-	BibNumber VARCHAR(10),
+	BIBNumber VARCHAR(10),
 	FirstName VARCHAR(100),
-	LastName VARCHAR(100),
+	LasName VARCHAR(100),
 	Town VARCHAR(100),
 	State VARCHAR(100)
 );
