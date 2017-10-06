b25e53ff66d02fdfe1cd89366f2b7f50745d43c5
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-05 19:46:31 -0700

    refactoring Wines Setup

--- WINE-setup.sql
+++ WINE-setup.sql
@@ -1,30 +1,32 @@
 -- aimccoy
 
 CREATE TABLE Appelations (
-	No CHAR(2) PRIMARY KEY,
-	Appelation VARCHAR(100),
-	County VARCHAR(100),
-	State VARCHAR(100),
-	Area VARCHAR(100),
-	isAva char(1)
+	No INTEGER PRIMARY KEY,
+	Appelation VARCHAR(50),
+	County VARCHAR(20),
+	State VARCHAR(15),
+	Area VARCHAR(25),
+	isAva VARCHAR(10)
 );
 
 CREATE TABLE Grapes (
-	ID CHAR(2) PRIMARY KEY,
-	Grape VARCHAR(100),
-	Color VARCHAR(100)
+	ID Integer PRIMARY KEY,
+	Grape VARCHAR(25),
+	Color VARCHAR(10)
 );
 
 CREATE TABLE Wine (
-	No CHAR(4) PRIMARY KEY,
-	Grape VARCHAR(100),
-	Winery VARCHAR(100),
-	Appelation VARCHAR(100),
-	State VARCHAR(100),
-	Name VARCHAR(100),
-	Year CHAR(4),
-	Price VARCHAR(3),
-	Score VARCHAR(2),
-	Cases VARCHAR(6),
-	Drink VARCHAR(5)
+	No INTEGER PRIMARY KEY,
+	Grape VARCHAR(25),
+	Winery VARCHAR(40),
+	Appelation VARCHAR(40),
+	State VARCHAR(15),
+	Name VARCHAR(60),
+	Year INTEGER,
+	Price INTEGER,
+	Score INTEGER,
+	Cases INTEGER,
+	Drink VARCHAR(10),
+	FOREIGN KEY (Grape) REFERENCES Grapes (ID),
+	FOREIGN KEY (Appelation) REFERENCES Appelations (Appelation)
 );
