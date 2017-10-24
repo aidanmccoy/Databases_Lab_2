9633bb83f79f994f3af50242ec7a1fb5d297fa5e
Aidan McCoy <aidanmccoy2@gmail.com>
2017-10-24 16:41:56 -0700

    Removes test

--- AIRLINES/bn.sh
+++ AIRLINES/bn.sh
@@ -5,4 +5,3 @@ mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-setup.sql
 mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airlines.sql
 mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airports100.sql
 mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-flights.sql
-mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-test.sql
