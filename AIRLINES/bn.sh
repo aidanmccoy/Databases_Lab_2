dbee8685288859c86b95a94eac761e8e10c9c593
Aidan McCoy <aidanmccoy2@gmail.com>
2017-10-24 16:46:00 -0700

    updated echos

--- AIRLINES/bn.sh
+++ AIRLINES/bn.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-cleanup.sql
-echo mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-setup.sql
-echo mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airlines.sql
-echo mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airports100.sql
-echo mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-flights.sql
+echo cleanup
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-cleanup.sql
+
+echo setup
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-setup.sql
+
+echo build airlines
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airlines.sql
+
+echo build airports
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airports100.sql
+
+echo build flights
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-flights.sql
