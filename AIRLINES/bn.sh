3101c6a91f1343637b27e3b84b4c36724130f658
Aidan McCoy <mccoy.aidan@gmail.com>
2017-10-24 16:29:47 -0700

    spelling errors

--- AIRLINES/bn.sh
+++ AIRLINES/bn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-cleanup.sql
-mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build.sql
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-setup.sql
 mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airlines.sql
-mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-airports100.sql
-mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-flights.sql
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-airports100.sql
+mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-build-flights.sql
 mysql -h csc365fall2017.webredirect.org aimccoy < AIRLINES-test.sql
